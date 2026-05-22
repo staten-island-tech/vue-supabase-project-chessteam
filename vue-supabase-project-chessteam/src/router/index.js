608d1b0744f38984a134eeda9c4acf3a56539e2b
JeanV155 <carlosgutierrez203074@gmail.com>
2026-05-22 12:30:58 -0400

    yyy

--- vue-supabase-project-chessteam/src/router/index.js
+++ vue-supabase-project-chessteam/src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ChessView from '@/views/ChessView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'chess',
+      component: ChessView,
+    },
+  ],
 })
 
 export default router
+
