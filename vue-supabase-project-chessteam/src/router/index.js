583d4d2ebb90b740a31f5fb1ae59f1bfcba3ecfc
JeanV155 <carlosgutierrez203074@gmail.com>
2026-06-05 12:32:15 -0400

    ffff

--- vue-supabase-project-chessteam/src/router/index.js
+++ vue-supabase-project-chessteam/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginViews from '../viewss/LoginViews.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: LoginViews,
+    },
+  ],
 })
 
 export default router
