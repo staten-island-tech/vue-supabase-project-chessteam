72b3a0557c8bd4408429c061167937f81f5d854a
Rassul-creator-commits <rassulk@nycstudents.net>
2026-06-30 01:47:44 -0400

    Update index.js

--- Vue-supabase-project-chessteam/src/router/index.js
+++ Vue-supabase-project-chessteam/src/router/index.js
@@ -8,30 +8,31 @@ import SignupViews from '../views/signupviews.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-routes: [
-  {
-    path: '/',
-    name: 'Login',
-    component: LoginViews,
-  },
-  {
-    path: '/signup',
-    name: 'Signup',
-    component: SignupViews,
-  },
-  {
-    path: '/chess',
-    name: 'chess',
-    component: ChessView,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/past-games',
-    name: 'past-games',
-    component: PastGamesView,
-    meta: { requiresAuth: true }
-  }
-],
+
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: LoginViews,
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: SignupViews,
+    },
+    {
+      path: '/chess',
+      name: 'chess',
+      component: ChessView,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/past-games',
+      name: 'past-games',
+      component: PastGamesView,
+      meta: { requiresAuth: true }
+    }
+  ],
 })
 
 router.beforeEach(async (to) => {
@@ -45,4 +46,5 @@ router.beforeEach(async (to) => {
 
   return true
 })
+
 export default router
